ae21e1606a8719f43e2c439052c87ff9387974fe
fractal <fractal@nebula-nook.ru>
2024-12-30 12:19:48 +0300

    feat: add Gruvbox theme (thanks to SophrTemin)

--- launcher/ui/themes/ThemeManager.cpp
+++ launcher/ui/themes/ThemeManager.cpp
@@ -28,6 +28,7 @@
 #include "Exception.h"
 #include "ui/themes/BrightTheme.h"
 #include "ui/themes/FreesmTheme.h"
+#include "ui/themes/GruvboxTheme.h"
 #include "ui/themes/CatPack.h"
 #include "ui/themes/CustomTheme.h"
 #include "ui/themes/DarkTheme.h"
@@ -135,6 +136,7 @@ void ThemeManager::initializeWidgets()
     themeDebugLog() << "Loading Built-in Theme:" << darkThemeId;
     themeDebugLog() << "Loading Built-in Theme:" << addTheme(std::make_unique<BrightTheme>());
     themeDebugLog() << "Loading Built-in Theme:" << addTheme(std::make_unique<FreesmTheme>());
+    themeDebugLog() << "Loading Built-in Theme:" << addTheme(std::make_unique<GruvboxTheme>());
 
     themeDebugLog() << "<> Initializing System Widget Themes";
     QStringList styles = QStyleFactory::keys();
