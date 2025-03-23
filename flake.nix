abd779d8823994c2f8076d28abf784a0971b9dd1
fractal <fractal@nebula-nook.ru>
2025-03-22 22:19:16 -0400

    build(nix): copy compile_commands.json when entering dev shell
    Signed-off-by: Seth Flynn <getchoo@tuta.io>

--- flake.nix
+++ flake.nix
@@ -79,6 +79,16 @@
               ccache
               ninja
             ];
+
+            cmakeFlags = self.packages.${system}.prismlauncher-unwrapped.cmakeFlags ++ [
+              "-GNinja"
+              "-Bbuild"
+            ];
+
+            shellHook = ''
+              cmake $cmakeFlags -D CMAKE_BUILD_TYPE=Debug
+              ln -s {build/,}compile_commands.json
+            '';
           };
         }
       );
