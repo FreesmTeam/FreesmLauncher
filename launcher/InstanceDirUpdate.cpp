cdb844163cda7dc142225aff644dc1d6b7b48daf
fractal <fractal@nebula-nook.ru>
2025-03-27 20:25:30 +0800

    Remove names from header comments
    Signed-off-by: Yihe Li <winmikedows@hotmail.com>

--- launcher/InstanceDirUpdate.cpp
+++ launcher/InstanceDirUpdate.cpp
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-only
 /*
  *  Prism Launcher - Minecraft Launcher
- *  Copyright (C) 2022 Sefa Eyeoglu <contact@scrumplex.net>
- *  Copyright (c) 2022 Jamie Mansfield <jmansfield@cadixdev.org>
- *  Copyright (C) 2023 TheKodeToad <TheKodeToad@proton.me>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
