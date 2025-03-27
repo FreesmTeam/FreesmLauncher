290e5eb956dd17cb85939c22a83e2a74280db375
fractal <fractal@nebula-nook.ru>
2025-03-27 00:37:39 +0000

    Try to make getchoo requested changes
    Signed-off-by: TheKodeToad <TheKodeToad@proton.me>

--- launcher/ui/widgets/MinecraftSettingsWidget.cpp
+++ launcher/ui/widgets/MinecraftSettingsWidget.cpp
@@ -50,14 +50,11 @@ MinecraftSettingsWidget::MinecraftSettingsWidget(MinecraftInstancePtr instance, 
     m_ui->setupUi(this);
 
     if (m_instance == nullptr) {
-        for (int i = m_ui->settingsTabs->count() - 1; i >= 0; --i) {
-            const QString name = m_ui->settingsTabs->widget(i)->objectName();
-
-            if (name == "javaPage" || name == "launchPage")
-                m_ui->settingsTabs->removeTab(i);
-        }
+        m_ui->settingsTabs->removeTab(1);
 
         m_ui->openGlobalSettingsButton->setVisible(false);
+        m_ui->instanceAccountGroupBox->hide();
+        m_ui->serverJoinGroupBox->hide();
     } else {
         m_javaSettings = new JavaSettingsWidget(m_instance, this);
         m_ui->javaScrollArea->setWidget(m_javaSettings);
