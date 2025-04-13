9a8c3d5d24ddfdd0af3ea80f3cfd2523c4cf358c
fractal <fractal@nebula-nook.ru>
2025-04-13 12:59:33 +0300

    fix(packaging): nixfmt-rfc-style migration to alejandra

--- flake.nix
+++ flake.nix
@@ -32,96 +32,85 @@
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      libnbtplusplus,
-      nix-filter,
-      ...
-    }:
-    let
-      inherit (nixpkgs) lib;
-      systems = lib.systems.flakeExposed;
-      forAllSystems = lib.genAttrs systems;
-      nixpkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
-    in
-    {
+  outputs = {
+    self,
+    nixpkgs,
+    libnbtplusplus,
+    nix-filter,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+    systems = lib.systems.flakeExposed;
+    forAllSystems = lib.genAttrs systems;
+    nixpkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
+  in {
+    formatter = forAllSystems (system: nixpkgsFor.${system}.alejandra);
 
-      formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt-rfc-style);
-
-      checks = forAllSystems (
-        system:
-        let
-          checks' = nixpkgsFor.${system}.callPackage ./nix/checks.nix { inherit self; };
-        in
+    checks = forAllSystems (
+      system: let
+        checks' = nixpkgsFor.${system}.callPackage ./nix/checks.nix {inherit self;};
+      in
         lib.filterAttrs (_: lib.isDerivation) checks'
-      );
+    );
 
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            inputsFrom = [
-              self.packages.${system}.freesmlauncher-unwrapped
-            ];
-            buildInputs = [
-              pkgs.ccache
-              pkgs.ninja
-            ];
-          };
-        }
-      );
-
-      overlays = {
-        default = final: prev: {
-          freesmlauncher-unwrapped = prev.callPackage ./nix/unwrapped.nix {
-            inherit
-              libnbtplusplus
-              nix-filter
-              self
-              ;
-          };
-
-          freesmlauncher = final.callPackage ./nix/wrapper.nix { };
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgsFor.${system};
+      in {
+        default = pkgs.mkShell {
+          inputsFrom = [
+            self.packages.${system}.freesmlauncher-unwrapped
+          ];
+          buildInputs = [
+            pkgs.ccache
+            pkgs.ninja
+          ];
         };
+      }
+    );
+
+    overlays = {
+      default = final: prev: {
+        freesmlauncher-unwrapped = prev.callPackage ./nix/unwrapped.nix {
+          inherit
+            libnbtplusplus
+            nix-filter
+            self
+            ;
+        };
+
+        freesmlauncher = final.callPackage ./nix/wrapper.nix {};
       };
-
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-
-          freesmPackages = lib.makeScope pkgs.newScope (final: self.overlays.default final pkgs);
-
-          packages = {
-            inherit (freesmPackages) freesmlauncher-unwrapped freesmlauncher;
-            default = freesmPackages.freesmlauncher;
-          };
-        in
-        lib.filterAttrs (_: lib.meta.availableOn pkgs.stdenv.hostPlatform) packages
-
-      );
-
-      legacyPackages = forAllSystems (
-        system:
-        let
-          freesmPackages = self.packages.${system};
-          legacyPackages = self.legacyPackages.${system};
-        in
-        {
-          freesmlauncher-debug = freesmPackages.freesmlauncher.override {
-            freesmlauncher-unwrapped = legacyPackages.freesmlauncher-unwrapped-debug;
-          };
-
-          freesmlauncher-unwrapped-debug = freesmPackages.freesmlauncher-unwrapped.overrideAttrs {
-            cmakeBuildType = "Debug";
-            dontStrip = true;
-          };
-        }
-      );
     };
+
+    packages = forAllSystems (
+      system: let
+        pkgs = nixpkgsFor.${system};
+
+        freesmPackages = lib.makeScope pkgs.newScope (final: self.overlays.default final pkgs);
+
+        packages = {
+          inherit (freesmPackages) freesmlauncher-unwrapped freesmlauncher;
+          default = freesmPackages.freesmlauncher;
+        };
+      in
+        lib.filterAttrs (_: lib.meta.availableOn pkgs.stdenv.hostPlatform) packages
+    );
+
+    legacyPackages = forAllSystems (
+      system: let
+        freesmPackages = self.packages.${system};
+        legacyPackages = self.legacyPackages.${system};
+      in {
+        freesmlauncher-debug = freesmPackages.freesmlauncher.override {
+          freesmlauncher-unwrapped = legacyPackages.freesmlauncher-unwrapped-debug;
+        };
+
+        freesmlauncher-unwrapped-debug = freesmPackages.freesmlauncher-unwrapped.overrideAttrs {
+          cmakeBuildType = "Debug";
+          dontStrip = true;
+        };
+      }
+    );
+  };
 }
