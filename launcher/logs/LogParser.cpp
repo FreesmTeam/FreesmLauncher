42ddde00eefadcf93ed3fa51cd583490a0d18e31
fractal <fractal@nebula-nook.ru>
2025-04-19 10:47:32 +0100

    Remove commented out code
    Signed-off-by: TheKodeToad <TheKodeToad@proton.me>

--- launcher/logs/LogParser.cpp
+++ launcher/logs/LogParser.cpp
@@ -362,12 +362,5 @@ MessageLevel::Enum LogParser::guessLevel(const QString& line, MessageLevel::Enum
     if (line.contains("overwriting existing"))
         return MessageLevel::Fatal;
 
-    // NOTE: this diverges from the real regexp. no unicode, the first section is + instead of *
-    // static const QRegularExpression JAVA_EXCEPTION(
-    //     R"(Exception in thread|...\d more$|(\s+at |Caused by:
-    //     )([a-zA-Z_$][a-zA-Z\d_$]*\.)+[a-zA-Z_$][a-zA-Z\d_$]*)|([a-zA-Z_$][a-zA-Z\d_$]*\.)+[a-zA-Z_$][a-zA-Z\d_$]*(Exception|Error|Throwable)");
-    //
-    // if (line.contains(JAVA_EXCEPTION))
-    //     return MessageLevel::Error;
     return MessageLevel::Info;
 }
