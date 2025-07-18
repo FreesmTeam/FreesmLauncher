214f22b80f58549f3f153d4f386d96525fbff876
fractal <fractal@nebula-nook.ru>
2025-07-18 18:22:46 +0300

    Update launcher/modplatform/ResourceType.h
    Co-authored-by: TheKodeToad <TheKodeToad@proton.me>
    Signed-off-by: Alexandru Ionut Tripon <alexandru.tripon97@gmail.com>

--- launcher/modplatform/ResourceType.h
+++ launcher/modplatform/ResourceType.h
@@ -32,7 +32,7 @@ namespace ModPlatform {
 enum class ResourceType { Mod, ResourcePack, ShaderPack, Modpack, DataPack, World, Screenshots, TexturePack, Unknown };
 
 namespace ResourceTypeUtils {
-static const std::set<ResourceType> ValidResources = { ResourceType::DataPack,   ResourceType::ResourcePack, ResourceType::TexturePack,
+static const std::set<ResourceType> VALID_RESOURCES = { ResourceType::DataPack,   ResourceType::ResourcePack, ResourceType::TexturePack,
                                                        ResourceType::ShaderPack, ResourceType::World,        ResourceType::Mod };
 QString getName(ResourceType type);
 }  // namespace ResourceTypeUtils
