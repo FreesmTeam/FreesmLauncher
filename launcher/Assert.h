25bb5aa77acce9f48c10b9451d10ad6b84133b5a
fractal <fractal@nebula-nook.ru>
2026-01-21 19:15:12 +0500

    code review
    Signed-off-by: Octol1ttle <l1ttleofficial@outlook.com>

--- launcher/Assert.h
+++ launcher/Assert.h
@@ -18,6 +18,8 @@
 
 #pragma once
 
-#if !defined(ASSERT_NEVER)
-#define ASSERT_NEVER(cond) (Q_ASSERT(cond == false), cond)
+#if defined(ASSERT_NEVER)
+#error ASSERT_NEVER already defined
+#else
+#define ASSERT_NEVER(cond) (Q_ASSERT((cond) == false), (cond))
 #endif
