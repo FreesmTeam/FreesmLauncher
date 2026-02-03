FreesmTeam/FreesmLauncher

commit 526d0c769e92c8532001d63fbcbd59a70e8a010f

Add icons to account list
Signed-off-by: TheKodeToad <TheKodeToad@proton.me>

launcher/minecraft/auth/AccountList.cpp
@@ -295,6 +295,24 @@ QVariant AccountList::data(const QModelIndex& index, int role) const
     MinecraftAccountPtr account = at(index.row());
 
     switch (role) {
+        case Qt::SizeHintRole:
+            if (index.column() == ProfileNameColumn) {
+                return QSize(0, 30);
+            }
+
+            return QVariant();
+        case Qt::DecorationRole:
+            if (index.column() == ProfileNameColumn) {
+                auto face = account->getFace(24, 24);
+
+                if (!face.isNull()) {
+                    return face;
+                } else {
+                    return QIcon::fromTheme("noaccount").pixmap(24, 24);
+                }
+            }
+
+            return QVariant();
         case Qt::DisplayRole:
             switch (index.column()) {
                 case ProfileNameColumn:

launcher/minecraft/auth/MinecraftAccount.cpp
@@ -101,7 +101,7 @@ AccountState MinecraftAccount::accountState() const
     return data.accountState;
 }
 
-QPixmap MinecraftAccount::getFace() const
+QPixmap MinecraftAccount::getFace(int width, int height) const
 {
     QPixmap skinTexture;
     if (!skinTexture.loadFromData(data.minecraftProfile.skin.data, "PNG")) {
@@ -112,7 +112,7 @@ QPixmap MinecraftAccount::getFace() const
     QPainter painter(&skin);
     painter.drawPixmap(0, 0, skinTexture.copy(8, 8, 8, 8));
     painter.drawPixmap(0, 0, skinTexture.copy(40, 8, 8, 8));
-    return skin.scaled(64, 64, Qt::KeepAspectRatio);
+    return skin.scaled(width, height, Qt::KeepAspectRatio);
 }
 
 shared_qobject_ptr<AuthFlow> MinecraftAccount::login(bool useDeviceCode)

launcher/minecraft/auth/MinecraftAccount.h
@@ -135,7 +135,7 @@ class MinecraftAccount : public QObject, public Usable {
         }
     }
 
-    QPixmap getFace() const;
+    QPixmap getFace(int width = 64, int height = 64) const;
 
     //! Returns the current state of the account
     AccountState accountState() const;

commit f6a9208d1c6ef491b27ca8bdad3ccf66ddac46cc

launcher/minecract/auth/AccountList.cpp: add missing QIcon include
In member function ‘virtual QVariant AccountList::data(const QModelIndex&, int) const’:
/var/tmp/portage/games-action/prismlauncher-9999/work/prismlauncher-9999/launcher/minecraft/auth/AccountList.cpp:331:35: error: incomplete type ‘QIcon’ used in nested name specifier
  331 |                     return QIcon::fromTheme("noaccount").pixmap(24, 24);
      |

Fixes: 526d0c769e92c8532001d63fbcbd59a70e8a010f
Signed-off-by: Alfred Wingate <parona@protonmail.com>

launcher/minecraft/auth/AccountList.cpp
@@ -39,6 +39,7 @@
 
 #include <QDir>
 #include <QFile>
+#include <QIcon>
 #include <QIODevice>
 #include <QJsonArray>
 #include <QJsonDocument>