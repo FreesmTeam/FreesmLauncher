b29a1a86a900cb2c01f65659cfa2d1dc21886cf5
fractal <fractal@nebula-nook.ru>
2026-02-12 19:20:05 +0300

    fix(ftb): change the response variable type

--- launcher/ui/pages/modplatform/ftb/FtbListModel.h
+++ launcher/ui/pages/modplatform/ftb/FtbListModel.h
@@ -77,7 +77,7 @@ private:
     NetJob::Ptr jobPtr;
     int currentPack;
     QList<int> remainingPacks;
-    QByteArray response;
+    std::shared_ptr<QByteArray> response = std::make_shared<QByteArray>();
 };
 
 }
